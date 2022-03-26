acb1584948857c80eaf374ebd1fa1950d770da95
naix7nab <tedxige@foxmail.com>
2022-03-26 08:42:42 +0800

    create new  project

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,16 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view />
+  hello wrold
+  <button @click="getMessage"></button>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script setup>
+import { get } from "@/service/base.js";
+
+function getMessage() {
+  get("/banner", {
+    type: 2,
+  }).then((res) => {
+    console.log(res);
+  });
 }
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+</script>
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,18 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
+  css: {
+    loaderOptions: {
+      sass: {
+        additionalData: `
+        @import '@/assets/scss/variable.scss';
+        @import '@/assets/scss/mixin.scss';
+        `
+      }
+    }
+  },
   lintOnSave: false,
+  devServer: {
+    proxy: 'https://autumnfish.cn/' // api请求地址
+  }
 });
